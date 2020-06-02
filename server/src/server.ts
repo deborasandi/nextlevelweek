cf34702568052607af995e70f6290bc11b10b06a
deborasandi <deborasandi@users.noreply.github.com>
2020-06-02 18:07:14 -0300

    finalização do back-end

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,15 @@
-import express from 'express';
+import express, { request, response } from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Listagem de Usuários')
+app.use(cors());
+app.use(express.json());
 
-    response.json([
-        'Débora',
-        'Yago',
-        'Picole',
-        'Sakura',
-        'Kiara'
-    ])
-});
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
