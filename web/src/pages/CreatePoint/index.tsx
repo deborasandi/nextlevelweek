2d8a042af27098170da291499c56aefaff443535
deborasandi <deborasandi@users.noreply.github.com>
2020-06-05 11:09:35 -0300

    Update do server

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -8,6 +8,7 @@ import {LeafletMouseEvent} from 'leaflet';
 
 import './styles.css';
 import logo from '../../assets/logo.svg';
+import DropZone from '../../components/dropzone'
 
 interface Item {
   id: number;
@@ -40,6 +41,7 @@ const CreatePoint = () => {
     whatsapp: '',
   });
   const [selectedItems, setSelectedItems] = useState<number[]>([]); 
+  const [selectedFile, setSelectedFile] = useState<File>(); 
 
   const history = useHistory();
 
@@ -123,16 +125,20 @@ const CreatePoint = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items
-    };
+    const data = new FormData()
+
+    data.append('name', name)
+    data.append('email', email)
+    data.append('whatsapp', whatsapp)
+    data.append('uf', uf)
+    data.append('city', city)
+    data.append('latitude', String(latitude))
+    data.append('longitude', String(longitude))
+    data.append('items', items.join(','))
+    
+    if(selectedFile){
+      data.append('image', selectedFile)
+    }
 
     await api.post('points', data);
 
@@ -153,6 +159,8 @@ const CreatePoint = () => {
 
       <form onSubmit={handleSubmit}>
         <h1>Cadastro do Ponto de Coleta</h1>
+
+        <DropZone onFileUploaded={setSelectedFile} />
 
         <fieldset>
           <legend>
